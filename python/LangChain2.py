9a287020c5828cd82c12c802ca045ba8f308a6d7
daedalus <13687739+daedalus1215@users.noreply.github.com>
2024-06-02 20:13:51 -0700

    add caching, so second call provides in memory cache response

--- python/LangChain2.py
+++ python/LangChain2.py
@@ -1,17 +1,24 @@
 from langchain_community.llms import Ollama # type: ignore
 from langchain.callbacks.manager import CallbackManager # type: ignore
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler # type: ignore
-from langchain.schema import AIMessage,HumanMessage,SystemMessage # type: ignore
+from langchain.schema import HumanMessage,SystemMessage # type: ignore
+from langchain_community.cache import InMemoryCache # type: ignore
+
 
 llm = Ollama(
     model="llama3", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
 )
+
+llm.cache = InMemoryCache()
+
 
 human_message = HumanMessage(content='Tell me a fact about Pluto')
 system_message = SystemMessage(content='you are a very rude teenager who won\t answer')
 #llm.invoke("Here is a fun fact about Pluto:")
 
 # Using __call__ directly
+results = llm.invoke([system_message, human_message],temperature=2, max_tokens=40) 
+
 results = llm.invoke([system_message, human_message],temperature=2, max_tokens=40) 
 
 print(results)
