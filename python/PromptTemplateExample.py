7e787139e97ef37e0cf00593d89cc2d5fbc99b84
daedalus <13687739+daedalus1215@users.noreply.github.com>
2024-06-05 19:01:07 -0700

    beginning to use the prompt template

--- python/PromptTemplateExample.py
+++ python/PromptTemplateExample.py
@@ -3,6 +3,7 @@ from langchain.callbacks.manager import CallbackManager # type: ignore
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler # type: ignore
 from langchain.schema import HumanMessage,SystemMessage # type: ignore
 from langchain_community.cache import InMemoryCache # type: ignore
+from langchain_core.prompts import PromptTemplate # type: ignore
 
 llm = Ollama(
     model="llama3", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
@@ -10,14 +11,7 @@ llm = Ollama(
 
 llm.cache = InMemoryCache()
 
+## Use templates
+single_input = PromptTemplate(input_variables=["topic"], template='Tell me a fact about {topic}')
 
-human_message = HumanMessage(content='Tell me a fact about Pluto')
-system_message = SystemMessage(content='you are a very rude teenager who won\t answer')
-#llm.invoke("Here is a fun fact about Pluto:")
-
-# Using __call__ directly
-results = llm.invoke([system_message, human_message],temperature=2, max_tokens=40) 
-
-results = llm.invoke([system_message, human_message],temperature=2, max_tokens=40) 
-
-print(results)
+llm.invoke(single_input.format(topic="Mars"))
