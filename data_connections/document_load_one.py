b7485c5cea05eae093b0af35eef1adee148d91fc
daedalus <13687739+daedalus1215@users.noreply.github.com>
2024-07-13 08:47:25 -0700

    PDF Loading

--- data_connections/document_load_one.py
+++ data_connections/document_load_one.py
@@ -1,5 +1,4 @@
-from langchain.document_loaders import CSVLoader, BSHTMLLoader
-
+from langchain.document_loaders import CSVLoader, BSHTMLLoader, PyPDFLoader
 
 loader = CSVLoader("data/penguins.csv")
 data = loader.load()
@@ -7,5 +6,9 @@ type(data[0])
 
 print(data[0].page_content)
 
-loader = BSHTMLLoader("data/some_website.html")
+html_loader = BSHTMLLoader("data/some_website.html")
 data = loader.load()
+
+pdf_loader = PyPDFLoader("data/SomeReport.pdf")
+
+print(pdf_loader.load()[0].page_content.replace('\n', ' '))
