493f382a6678b83ca382368bf59d7afa60fe4939
daedalus <13687739+daedalus1215@users.noreply.github.com>
2024-07-25 19:15:22 -0700

    Chroma_db is working now

--- data_connections/chroma_db.py
+++ data_connections/chroma_db.py
@@ -2,22 +2,9 @@ from langchain_chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from llama_index.embeddings.langchain import LangchainEmbedding
 from langchain_community.document_loaders import CSVLoader
-
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader
-
-
-
-# LOAD DOCUMENT --> SPLIT INTO CHUNK
-
-
-# EMBEDDING --> EMBED CHUNKS --> VECTORS
-
-# VECT OR CHUNKS --> SAVE CHROMADB
-
-# "query" --> Similarity search chromadb
-
 
 loader = TextLoader('data/FDR_State_of_Union_1944.txt')
 documents = loader.load()
@@ -27,7 +14,6 @@ docs = text_splitter.split_documents(documents)
 # docs
 embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-
 db = Chroma.from_documents(docs, embedding_function, persist_directory="./speech_new_db")
 
 db.persist() # tell chroma to save it.
@@ -35,16 +21,14 @@ db.persist() # tell chroma to save it.
 
 # pull it out of the database
 db_new_connection = Chroma(persist_directory="./speech_new_db", embedding_function=embedding_function)
-new_doc = 'What did FDR say about the cost of food law?'
-similar_docs = db_new_connection.similarity_search(new_doc)
+similar_docs = db_new_connection.similarity_search('What did FDR say about the cost of food law?')
 print(similar_docs[0].page_content)
-
 
 loader = TextLoader("data/lincoln_state_of_union.txt")
 documents = loader.load()
 docs = text_splitter.split_documents(documents)
 db_new_connection = Chroma.from_documents(docs, embedding_function, persist_directory='./speech_new_db')
-blocks = db_new_connection.similarity_search('cost of food')
+blocks = db_new_connection.similarity_search('said of the proportion')
 
 # print(blocks[0].page_content)
 print(blocks[0].metadata)
