3687f480959a5be862e1d4bfe62386c635193b95
daedalus <13687739+daedalus1215@users.noreply.github.com>
2024-08-01 22:43:53 -0700

    Get the first LLMChain wrapper to work

--- chains/llmchain_object.py
+++ chains/llmchain_object.py
@@ -1,16 +1,12 @@
-from chains.ollama_adapter import OllamaAdapter
-from langchain.callbacks.manager import CallbackManager # type: ignore
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler # type: ignore
-from langchain_community.llms import Ollama # type: ignore
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate # type: ignore
-from langchain.output_parsers import CommaSeparatedListOutputParser
-from langchain_community.document_loaders import WikipediaLoader
 from langchain.chains import LLMChain
+
+from ollama_factory import OllamaFactory
 
 human_prompt = HumanMessagePromptTemplate.from_template('Make up a funny company name, for a company that makes: {product}')
 chat_prompt_template = ChatPromptTemplate.from_messages([human_prompt]);
 
-chat = OllamaAdapter()
+chat = OllamaFactory().create_llm()
 
 chain = LLMChain(llm=chat, prompt=chat_prompt_template)
 
--- chains/ollama_factory.py
+++ chains/ollama_factory.py
@@ -2,10 +2,8 @@ from langchain.callbacks.manager import CallbackManager # type: ignore
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler # type: ignore
 from langchain_community.llms import Ollama # type: ignore
 
-class OllamaAdapter:
-    def __init__(self):
-        llm = Ollama(
+class OllamaFactory:
+    def create_llm(self):
+        return Ollama(
             model="llama3", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
         )
-    
-    
