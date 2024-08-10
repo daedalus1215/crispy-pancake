0750d0230c1dcd3da2f0fb8f31d073fc26f0665a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-10 15:22:54 -0400

    leverage chroma to check the 14th amendment.

--- chains/additional_chains.py
+++ chains/additional_chains.py
@@ -12,11 +12,12 @@ from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
 # docs
 llm = OllamaFactory().create_llm()
+# Need to run us-constitution-bot.py to generate the solutions folder
 embedding_function = OllamaFactory.create_embedding_function()
-db_connection = OllamaFactory.create_db_connection('../data_connections/data/US_Constitution',
+db_connection = OllamaFactory.create_db_connection('../data_connections/solution.',
                                    embedded_function=embedding_function)
 
-chain = load_qa_chain(llm, chain_type='stuff') # stuff = inserting context into our model (pulled later on from choma store)
-question = "What is the 15th amendment?"
+chain = load_qa_with_sources_chain(llm, chain_type='stuff') # stuff = inserting context into our model (pulled later on from choma store)
+question = "What is the 14th amendment?"
 docs = db_connection.similarity_search(question)
 chain.run(input_documents=docs, question=question)
