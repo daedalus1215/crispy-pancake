1123a1013376488df196736a976df3d8298d3c3c
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-17 09:00:46 -0400

    memory: bring in pickle for memory buffering

--- memory/chat_message_buffering_history.py
+++ memory/chat_message_buffering_history.py
@@ -1,6 +1,7 @@
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from ollama_factory import OllamaFactory
+import pickle
 
 llm = OllamaFactory().create_llm()
 memory = ConversationBufferMemory()
@@ -8,3 +9,19 @@ conversation = ConversationChain(llm = llm, memory = memory, verbose=True)
 conversation.predict(input='Hello, nice to meet you')
 
 conversation.predict(input="Tell me about an interesting physic facts")
+
+memory.load_memory_variables({})
+
+# Display the conversation memory
+conversation.memory
+
+pickled_str = pickle.dumps(conversation.memory)
+
+with open('convo_memory.pkl', 'wb') as f:
+    f.write(pickled_str)
+
+llm = OllamaFactory.create_llm()
+
+new_memory_loaded = pickle.loads(open('convo_memory.pkl', 'rb').read())
+reload_conversation = ConversationChain(llm=llm, memory=new_memory_loaded)
+print(reload_conversation.memory_buffer)
