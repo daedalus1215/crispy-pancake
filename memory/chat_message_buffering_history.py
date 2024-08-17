166241a98bc86e53c98eaf5bf829ab21df312cb0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-17 08:49:04 -0400

    memory: set up chat_message_buffer

---
+++ memory/chat_message_buffering_history.py
@@ -0,0 +1,9 @@
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from ollama_factory import OllamaFactory
+
+llm = OllamaFactory().create_llm()
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm = llm, memory = memory, verbose=True)
+conversation.predict(input='Hello, nice to meet you')
+
